c9bafda23b66725a54146e326d7f60ed3b6bddd9
Rail Udemy Course <serliichong@gmail.com>
2019-10-11 17:11:50 +0700

    Make app ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'welcome/page', to: 'welcome#page'
-  get 'welcome/about', to: 'welcome#about'
+  root 'welcome#page'
+  get 'about', to: 'welcome#about'
 
 end
